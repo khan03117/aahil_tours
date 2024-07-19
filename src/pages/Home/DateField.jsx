0acf300fb9dfdaa4e8f7725d60fdb6e4e22813cd
= <=>
2024-07-19 22:39:42 +0530

    main

--- src/pages/Home/DateField.jsx
+++ src/pages/Home/DateField.jsx
@@ -1,20 +1,32 @@
 // import React from 'react'
 import PropTypes from 'prop-types';
 import LabelSearch from './LabelSearch';
-const DateField = ({ label }) => {
+import { CloseOutlined } from '@ant-design/icons';
+const DateField = ({ label, disabled, handletrip }) => {
   return (
     <>
       <div className="w-full bg-white h-full p-3 relative">
         <LabelSearch label={label} />
         <div className="w-full">
-          <input type="date" className="w-full z-40 date-input absolute top-0 p-2 outline-none start-0 h-full bg-transparent" />
+          {
+            disabled && (
+              <>
+                <button onClick={() => handletrip(2)} className='absolute top-2 z-50 end-2 size-6 text-gray-600'>
+                  <CloseOutlined />
+                </button>
+              </>
+            )
+          }
+          <input type="date" disabled={disabled} className="w-full z-40 date-input absolute top-0 p-2 outline-none start-0 h-full bg-transparent" />
         </div>
       </div>
     </>
   )
 }
 DateField.propTypes = {
-  label: PropTypes.string
+  label: PropTypes.string,
+  disabled: PropTypes.bool,
+  handletrip: PropTypes.func
 }
 
 export default DateField
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,17 +5,35 @@ import { useState } from "react"
 import DateField from "./DateField"
 import FromField from "./FromField"
 import LabelSearch from "./LabelSearch"
+import { CloseCircleFilled, PlusOutlined } from "@ant-design/icons"
 
 const Home = () => {
-    const [btn,setBtn] = useState('')
-    const [quota, setQuota] = useState('')
+    const [trip, setTrip] = useState(1)
+    const [quota, setQuota] = useState('');
+    const [rows, setRows] = useState(1);
+    const trips = [
+        { id: 1, trip: "One Way" },
+        { id: 2, trip: "Round Trip" },
+        { id: 3, trip: "Mulicity" }
+    ];
     const checkinquota = (itm) => {
-        setQuota(itm)
+        setQuota(itm);
     }
-    const checkin = (itm) =>{
-        setBtn(itm) 
+    const handletrip = (itm) => {
+        setTrip(itm)
+        if (itm == 3) {
+            setRows((prev) => (prev > 2 ? prev : 2));
+        } else {
+            setRows(1);
+        }
     }
 
+    const addcity = () => {
+        setRows((prev) => (prev + 1))
+    }
+    const removecity = () => {
+        setRows((prev) => Math.max(prev -1 , 1))
+    }
 
     return (
         <>
@@ -25,43 +43,86 @@ const Home = () => {
                         <div className="col-span-1">
                             <div className="w-full mb-3">
                                 {
-                                    ["One Way", "Round Trip", "Multicity"].map((itm) => (
+                                    trips.map((itm) => (
                                         <>
-                                            <button onClick={() => checkin(itm)} className={`text-sm text-gray-300 font-bold px-4 py-1 ${btn == itm ?"bg-white text-primary rounded-full":""}`}>{itm}</button>
+                                            <button onClick={() => handletrip(itm.id)} className={`text-sm text-gray-300 font-bold px-4 py-1 ${trip == itm.id ? "bg-white text-primary rounded-full" : ""}`}>{itm.trip}</button>
                                         </>
                                     ))
                                 }
                             </div>
                         </div>
                     </div>
-                    <div className="grid grid-cols-8  *:border-e *:border-blue-gray-100">
-                        <div className="col-span-2 rounded-s">
-                            <FromField label="From" />
-                        </div>
-                        <div className="col-span-2">
-                            <FromField label="To" />
-                        </div>
-                        <div className="col-span-1">
-                            <DateField label={"Departure Date"} />
-                        </div>
-                        <div className="col-span-1">
-                            <DateField label={"Return Date"} />
-                        </div>
-                        <div className="col-span-1">
-                            <div className="w-full p-3 bg-white h-full">
-                                <LabelSearch label={"Traveller & Class"} />
-                                <h4>
-                                    <span className="text-xl font-bold me-1">1</span>
-                                    <span className="text-sm me-1">July</span>
-                                    <span>2024</span>
-                                </h4>
-                            </div>
-                        </div>
-                        <div className="col-span-1">
-                            <button className="w-full h-full  rounded-e bg-orange-900 text-white outline-none">Search</button>
-                        </div>
+                    {
+                        [...Array(rows)].map((a, index) => (
+                            <>
+                                <div key={a} className="grid border-b border-blue-gray-100 last:border-none grid-cols-8  *:border-e *:border-blue-gray-100">
+                                    <div className="col-span-2 rounded-s">
+                                        <FromField label="From" />
+                                    </div>
+                                    <div className="col-span-2">
+                                        <FromField label="To" />
+                                    </div>
+                                    <div className="col-span-1">
+                                        <DateField handletrip={handletrip} disabled={false} label={"Departure Date"} />
+                                    </div>
+                                    {
+                                        trip != 3 && (
+                                            <>
+                                                <div className="col-span-1">
+                                                    <DateField handletrip={handletrip} label={"Return Date"} disabled={trip == 2 ? false : true} />
+                                                </div>
+                                            </>
+                                        )
+                                    }
+                                    {
+                                        index == 0 && (
+                                            <>
+                                                <div className="col-span-1 border-none">
+                                                    <div className="w-full p-3 bg-white h-full">
+                                                        <LabelSearch label={"Traveller & Class"} />
+                                                        <h4>
+                                                            <span className="text-xl font-bold me-1">1</span>
+                                                            <span className="text-sm me-1">July</span>
+                                                            <span>2024</span>
+                                                        </h4>
+                                                    </div>
+                                                </div>
+                                            </>
+                                        )
+                                    }
 
-                    </div>
+                                    <div className={`${trip != 3 ? 'col-span-1' : index == 0 ? 'col-span-2' : 'col-span-3'}  border-none bg-white`}>
+                                        {
+                                            index + 1 == rows ? <>
+                                                <div className={`w-full h-full ${trip != 3 ? '' : 'p-3 flex gap-2 items-center'}`}>
+                                                    <button className={`px-7 text-sm ${trip != 3 ? 'h-full w-full rounded-e' : 'py-2 rounded-full'}   bg-orange-900 text-white outline-none`}>Search</button>
+                                                    <button onClick={addcity} className="text-primary px-4 py-2 text-sm border border-primary rounded-full">
+                                                        <PlusOutlined/> Add City
+                                                    </button>
+                                                    {
+                                                        index > 1 && (
+                                                            <>
+                                                                <button onClick={removecity} className=" text-xl text-gray-800">
+                                                        <CloseCircleFilled/>
+                                                    </button>
+                                                            </>
+                                                        )
+                                                    }
+                                                
+                                                 
+                                                </div>
+
+                                            </> : <>
+
+                                            </>
+                                        }
+                                    </div>
+                                  
+
+                                </div>
+                            </>
+                        ))
+                    }
                     <div className="grid grid-cols-1">
 
                         <div className="col-span-1">
