f33769ff0619161a3eaa231892f7d01275512fa3
= <=>
2024-07-19 09:18:27 +0530

    mnb

--- src/pages/Home/DateField.jsx
+++ src/pages/Home/DateField.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+// import React from 'react'
 import PropTypes from 'prop-types';
-const DateField = () => {
+import LabelSearch from './LabelSearch';
+const DateField = ({ label }) => {
   return (
-    <div>DateField</div>
+    <>
+      <div className="w-full bg-white h-full p-3 relative">
+        <LabelSearch label={label} />
+        <div className="w-full">
+          <input type="date" className="w-full z-40 date-input absolute top-0 p-2 outline-none start-0 h-full bg-transparent" />
+        </div>
+      </div>
+    </>
   )
 }
 DateField.propTypes = {
-
+  label: PropTypes.string
 }
 
 export default DateField
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,41 @@
 // import React from 'react'
 
+import DateField from "./DateField"
 import FromField from "./FromField"
+import LabelSearch from "./LabelSearch"
 
 const Home = () => {
     return (
         <>
             <section className="bg-primary p-10">
-
-
                 <div className="container mx-auto">
-                    <div className="grid grid-cols-8 *:border-e *:border-blue-gray-100">
-                        <div className="col-span-2">
+                    <div className="grid grid-cols-8  *:border-e *:border-blue-gray-100">
+                        <div className="col-span-2 rounded-s">
                             <FromField label="From" />
                         </div>
                         <div className="col-span-2">
                             <FromField label="To" />
                         </div>
+                        <div className="col-span-1">
+                            <DateField label={"Departure Date"} />
+                        </div>
+                        <div className="col-span-1">
+                            <DateField label={"Return Date"} />
+                        </div>
+                        <div className="col-span-1">
+                            <div className="w-full p-3 bg-white h-full">
+                                <LabelSearch label={"Traveller & Class"} />
+                                <h4>
+                                    <span className="text-xl font-bold me-1">1</span>
+                                    <span className="text-sm me-1">July</span>
+                                    <span>2024</span>
+                                </h4>
+                            </div>
+                        </div>
+                        <div className="col-span-1">
+                            <button className="w-full h-full  rounded-e bg-orange-900 text-white outline-none">Search</button>
+                        </div>
+
                     </div>
                 </div>
             </section>
