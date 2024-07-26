f86787740b3afce6feb5fa06ed48e8b9ad9df043
mndev2019 <muskanvision2019@gmail.com>
2024-07-26 10:43:12 +0530

    h

---
+++ src/pages/SearchFlightResult/AddPassengerDetails.jsx
@@ -0,0 +1,51 @@
+//import React from 'react'
+
+const AddPassengerDetails = () => {
+    return (
+     <>
+       <section className="py-5">
+          <div className="container mx-auto">
+                <div className="grid grid-cols-12">
+                   <div className="col-span-9">
+                      <div className="w-full">
+  
+                      </div>
+                   </div>
+                   <div className="col-span-3">
+                      <div className="w-full">
+                          <p className="text-gray-400 text-md ">Fare Summary</p>
+                          <table className="w-full table">
+                              <tbody className="*:text-sm font-semibold">
+                                  <tr className="border-b-2 border-gray-200">
+                                      <td>Base fare</td>
+                                      <td className="text-end">$5,568.00</td>
+                                  </tr>
+                                  <tr className="border-b-2 border-gray-200">
+                                      <td>Base fare</td>
+                                      <td className="text-end">$5,568.00</td>
+                                  </tr>
+                                  <tr className="border-b-2 border-gray-200">
+                                      <td>Base fare</td>
+                                      <td className="text-end">$5,568.00</td>
+                                  </tr>
+                                  <tr className="border-b-2 border-gray-200">
+                                      <td>Base fare</td>
+                                      <td className="text-end">$5,568.00</td>
+                                  </tr>
+                                  <tr className="border-b-2 border-gray-200">
+                                      <td>Base fare</td>
+                                      <td className="text-end">$5,568.00</td>
+                                  </tr>
+                                  
+                              </tbody>
+                          </table>
+                      </div>
+                   </div>
+                </div>
+          </div>
+       </section>
+     </>
+    )
+  }
+  
+  export default AddPassengerDetails
