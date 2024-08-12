6f62c3b58ae6b7c0346229e82f1ac67310ab9561
= <=>
2024-08-12 08:29:37 +0530

    "Updated App.jsx to add new routes and AgencyLayout, and modified BASE_URL in Utils.js"

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider,  } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, } from 'react-router-dom'
 import './App.css'
 import Layout from './layout'
 import Home from './pages/Home'
@@ -8,21 +8,26 @@ import SearchFlightsRes from './pages/SearchFlightResult/SearchFlightsRes'
 import Review from './pages/Review'
 import AddPassengerDetails from './pages/Review/AddPassengerDetails'
 import PdfViewer from './ticket/PdfViewer'
+import AgencyLayout from './airiq/layout/AgencyLayout'
+import Login from './airiq/auth/Login'
 
 function App() {
 
   const themeRoutes = createBrowserRouter(
     createRoutesFromElements(
       <>
-      <Route  path="/" element={< Layout />} >
-      <Route index element={<Home />} />
-      <Route path='/hotel-list' element={<HotelLists/>}/>
-      <Route path='/single-list' element={<SingleHotel/>}/>
-      <Route path='/search-flight' element={<SearchFlightsRes/>}/>
-      <Route path='/review/:id' element={<Review/>} />
-      <Route path='passenger-details/:id' element={<AddPassengerDetails/>}/>
-      <Route path='/ticket/:id' element={<PdfViewer/>} />
-      </Route>
+        <Route path="/" element={< Layout />} >
+          <Route index element={<Home />} />
+          <Route path='/hotel-list' element={<HotelLists />} />
+          <Route path='/single-list' element={<SingleHotel />} />
+          <Route path='/search-flight' element={<SearchFlightsRes />} />
+          <Route path='/review/:id' element={<Review />} />
+          <Route path='passenger-details/:id' element={<AddPassengerDetails />} />
+          <Route path='/ticket/:id' element={<PdfViewer />} />
+        </Route>
+        <Route path='' element={<AgencyLayout />}>
+          <Route path='/agency/login' element={<Login />} />
+        </Route>
       </>
     )
   )
---
+++ src/airiq/auth/Login.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { Button, Input } from "@material-tailwind/react"
+import { postData } from '../../Utils';
+
+const Login = () => {
+    const [fdata, setFdata] = React.useState({username : "", password : ""});
+    const handleFdata = (e) => {
+        setFdata((prev) => ({...prev, 
+            [e.target.name] : e.target.value
+        }));
+    }
+
+    const login = async (e) => {
+        e.preventDefault();
+        try {
+            const item = await postData('agency/login', fdata);
+            console.log(item);
+        }catch(err){
+            console.log(err);
+        }
+    }
+    return (
+        <>
+            <section>
+                <div className="container mx-auto">
+                    <div className="grid grid-cols-2">
+
+                    </div>
+                </div>
+            </section>
+            <section className="py-5">
+                <div className="container mx-auto">
+                    <div className="grid grid-cols-12 items-center">
+                        <div className="col-span-8">
+                            <div className="w-full">
+                                <img src="https://5.imimg.com/data5/JU/SF/QE/SELLER-12078893/fly-ticket-500x500.jpg" alt="" className="max-w-full" />
+                            </div>
+                        </div>
+                        <div className="col-span-4">
+                            <div className="w-full  rounded shadow-sm shadow-blue-gray-400 p-4 border border-blue-gray-300">
+                                <h2 className="text-2xl text-blue-gray-900 font-bold mb-4">Agency Login</h2>
+                                <div className="w-full">
+                                    <form onSubmit={login} className="w-full">
+                                        <div className="w-full mb-4">
+                                            <Input type="text" name='username' onChange={handleFdata} label="Enter Username" required />
+                                        </div>
+                                        <div className="w-full mb-4">
+                                            <Input type="password" name='password' onChange={handleFdata} label="Enter Password" required />
+                                        </div>
+                                        <div className="w-full">
+                                            <Button type="submit" value={'Login'} color="blue" fullWidth variant="gradient" >Login</Button>
+                                        </div>
+                                    </form>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="w-full">
+                <div className="container mx-auto">
+                    <div className="grid grid-cols-12">
+                        <div className="col-span-12">
+                            <div className="w-full p-4 bg-primary text-white">
+                                <p className="text-sm">Copyright 2022. All rights reserved.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Login
